02a502e7b9a67626954a3a76e02dc488fded1bbb
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-11 16:34:08 +0000

    variables but with a bug

---
+++ basics_01/variables_01.js
@@ -0,0 +1,15 @@
+const accountid = 112233
+let accountemail = "dheeru@gmail.com"
+var accountpassword = "0000"
+let account city = "jamnagar"
+
+// accountid = 12345
+
+accountemail = "dj@djgmail.com"
+accountpassword = "1111"
+accountcity = "jabalpur"
+
+
+console.log(accountid);
+
+console.table([accountid, accountemail, accountpassword, accountcity])
