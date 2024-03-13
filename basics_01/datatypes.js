1c177b094648200e6caa5fb787b046ae22379b4a
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-13 04:11:58 +0000

    learning of datatypes

---
+++ basics_01/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"
+// treat all js code as newer version
+
+// alert (3 + 3) 
+// we are using node js not browser
+
+console.log( 3
+    + 
+    3);
+    //  code readability is imp 
+
+console.log("dheeraj");
+
+let name = "dheeraj"
+let age = 18
+let isloggedin = false
+
+// diffrent types of datatypes:-
+// Number = 2 to power 53
+// bigint = for big numbers 
+// string = "name etc"
+// boolen = true or false 
+// null = standalone value (object)
+// undefined= no value given 
+// symbol = show uniqueness
+// object
+
+console.log(typeof "14");
+console.log(typeof null);
+// object datatype
+console.log(typeof undefined); 
+// undefined datatype 
+
