abfdfd4e0793d5aa67692c708cd85015c31d19ab
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-14 17:04:37 +0000

    conversion operation but a bug

---
+++ basics_01/conversion operation_01.js
@@ -0,0 +1,7 @@
+let score = 33;
+
+console.log(typeof score); // Output: number
+console.log(typeof (score)); // Output: number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // Output: number
