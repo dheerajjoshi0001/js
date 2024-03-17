0ddf2ad25ee089a9d55584754cbb4a17577c4be5
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-17 05:29:21 +0000

    operation learning

---
+++ basics_01/operation.js
@@ -0,0 +1,41 @@
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(4/2);
+// console.log(4%2);
+
+let str1 ="hello"
+let str2 =" dheeraj"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+ 2 +2); //output= 122 (if string is first then is writeen normal)
+console.log(1 + 2 + "2"); // output = 32 (if string is lst then first no added up)
+
+console.log( 3 + 4 * 5 % 2); // dont write such confusing code 
+// code shoulb be simple and easy readable
+
+// console.log(+true);
+// console.log(+""); // never do such type of conversion or operation
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2 // dont write such confusing code
+
+let gameCounter = 100
+++gameCounter;
+// gameCounter++; // learn more about prefix and postfix from MDN
+
+console.log(gameCounter);
+
+
+
+
