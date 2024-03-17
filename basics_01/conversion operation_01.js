1ba42da3fec736481109216bd2f82a2acfb9554c
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-17 04:26:51 +0000

    conversion of datatypes

--- basics_01/conversion operation_01.js
+++ basics_01/conversion operation_01.js
@@ -3,5 +3,21 @@ let score = 33;
 console.log(typeof score); // Output: number
 console.log(typeof (score)); // Output: number
 
-let valueInNumber = Number(score);
+let score2 = "33"
+
+console.log(typeof score); //output: string
+console.log(typeof (score)); // output: string
+
+let valueInNumber = Number(score2);
 console.log(typeof valueInNumber); // Output: number
+console.log(valueinNumber); // NaN
+
+// IF score or value is null then result will 0 and 
+//if value is undefined then the result will be naN 
+
+//true is 1 and false is 0
+// any string name value will NaN IN RESULT
+
+// 1=>TRUE 0=>false 
+// "empty string"=>false
+// "filled string"=>true
