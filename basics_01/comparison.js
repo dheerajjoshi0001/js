c7b13881c04552d2845f1712fc2c34198e59e527
dheeraj <157495125+dheerajjoshi0001@users.noreply.github.com>
2024-03-18 02:45:17 +0000

    try diff comparision

---
+++ basics_01/comparison.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(3 >= (2-1));
+console.log(2 <= 4);
+console.log(2 != 1); // datatypes are same so the conversion is simple
+
+console.log("2" > 1);
+console.log("02" < 3); // datatypes are different 
+
+console.log(null < 0);
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null <= 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined <= 0); //this is comfusing code.
+// comparison shoulb be simple and easily understandable
+
+// === (strict cjeck or triple check) 
+// it not only check number but also the datatypes
+
