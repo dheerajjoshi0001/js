616ae4df04f178b80b154f02ebfda6322cd0d62a
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-18 20:38:54 +0530

    basic operation in js

---
+++ basics/operation.js
@@ -0,0 +1,39 @@
+let value = 6 
+let negvalue = -value
+
+console.log(value);
+
+console.log(negvalue);
+
+console.log( 2 + 2);
+console.log( 2 - 2);
+console.log( 2 * 2);
+console.log( 2 ** 2);  //power
+console.log( 2 / 2);
+console.log( 2 % 2); //modules remainder
+
+let str1 = "hello"
+let str2 = " dheeraj"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log( 1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log( 1 + 2 + "2");  //32 
+console.log( 1 + "2" + "2");  //122
+
+let gamecounter = 99
+console.log(gamecounter);
+
+gamecounter++
+console.log(gamecounter);
+
+// learn about prefix and postfix from mdn or tc39.es 
+
+
+
+
+
