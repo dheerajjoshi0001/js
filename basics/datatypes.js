1f111fe897c76fcac139627aa2e18cc7d58443fd
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-18 17:11:38 +0530

    primitive datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as a newer version of js 
+
+// alert ( 2 + 3) we are using node not browser
+
+// code readibilty should be high
+
+// mdn and tc39.es are the best source for js documentation
+
+// datatypes.....................
+
+// Number => 2 to power 53
+// bigint
+// string ""
+// boolean => true or false
+// null = standalone value empty
+// undefined => no value
+// symbol => unique value 
+
+// object => key value pair  // primitive data types
+
+console.log(typeof "trijal");
+console.log(typeof null); // object
+console.log(typeof undefined);  // undefined
+console.log(typeof 23);
+
