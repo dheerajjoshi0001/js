03b95251ce04523ec8790e03bcd8a21658751e7e
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-18 16:55:41 +0530

    story of let var and const

---
+++ basics/var.js
@@ -0,0 +1,22 @@
+const accountid = 1234
+let name = 'trijal'
+var age = 23
+city = 'kashipur'
+let state;
+
+// accountid = 2 not allowed
+
+// accountid = 123 it is constant.
+name = 'himanshu'
+age = 24
+city = 'chati'
+
+console.log(accountid);
+
+console.table([accountid, name, age, city, state])
+
+// only use const and let 
+// not to use var because of issue in block scope and functional scope
+
+
+
