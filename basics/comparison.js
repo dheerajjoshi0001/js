fb001fe79a0483de2ac1f91b8e6a248822d40bdd
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-18 20:54:06 +0530

    comparison in js b/w datatypes

---
+++ basics/comparison.js
@@ -0,0 +1,32 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+console.log(1 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+// comparison and euality check are different and work differently in js 
+
+//  ==  euality check
+//=== strict euality check meand datatypes shold also be same with value
+
+
+
+console.log("2" === 1); //false
+console.log( 2 === 2); //true
+
