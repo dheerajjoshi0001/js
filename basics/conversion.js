3c3ec0637e80ad2f9dbf89c37cb4cf9a545ecf50
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-18 20:22:28 +0530

    basic conversion in js

---
+++ basics/conversion.js
@@ -0,0 +1,39 @@
+let score = "33";
+
+console.log(typeof score);
+// console.log(typeof (score));  // both are right 
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// let score = null;
+
+// console.log(typeof score);  // object
+// // console.log(typeof (score));  // both are right 
+
+// let valueinnumber = Number(score)
+
+// console.log(typeof valueinnumber); // number
+// console.log(valueinnumber);  // 0
+
+
+// "33" = 33 
+// "33abc" = nan
+// true = 1 , false = 0
+
+// 1 or any num. = true , 0 or empty string = false 
+
+let number = 33
+
+console.log(typeof number);
+
+
+Stringnumber = String(number)
+
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
+
+
+
