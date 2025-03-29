542cc1f13806937969c507e47f5b19ac513065ec
dheeraj <dhirajjoshi.987@gmail.com>
2025-03-29 08:57:02 +0530

    if else in js

---
+++ controlflow/1.js
@@ -0,0 +1,52 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }  // this is the old way to write the if else statement
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==  //comparison operators
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);  // this will not work because power is not defined outside the if block
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");  //if elseif else statement
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}  // true only if all the conditions are true
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}  // true if any of the conditions are true
