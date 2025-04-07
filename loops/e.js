744cb34b9c4c0b641de43c001f64c8833de4ec1a
dheeraj <dhirajjoshi.987@gmail.com>
2025-04-07 08:14:42 +0530

    loop array by for each

--- loops/e.js
+++ loops/e.js
@@ -1,20 +1,41 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-coding.forEach( function (val){
-    console.log(val);
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// } //used to print the array elements one by one
+
+// coding.forEach(printMe)  // //used to print the array elements one by one
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } ) //used to get the index and array in forEach loop
+
+// --------------------------------******-----------------------------------
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
 } )
-
-coding.forEach( (item) => {
-    console.log(item);
-} )
-
-function printMe(item){
-    console.log(item);
-} //used to print the array elements one by one
-
-coding.forEach(printMe)  // //used to print the array elements one by one
-
-coding.forEach( (item, index, arr)=> {
-    console.log(item, index, arr);
-} ) //used to get the index and array in forEach loop
-
